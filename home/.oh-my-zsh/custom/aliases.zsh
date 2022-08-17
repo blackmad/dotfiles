f5f0528ef17fa13b202898f8e998b89f9760347e
David Blackman <blackmad@makenotion.com>
2022-08-17 14:16:11 -0400

    stuff

--- home/.oh-my-zsh/custom/aliases.zsh
+++ home/.oh-my-zsh/custom/aliases.zsh
@@ -2,7 +2,6 @@
 ### git
 #####################
 
-
 function git_guess_main_branch_name {
   if git show-ref --quiet refs/heads/main; then
     echo -n main
@@ -15,7 +14,7 @@ function gc {
   if [[ $1 ]]; then
     git checkout $1
   else
-    git checkout "`git branch | cut -c 3- | fzf --preview="git log {} --"`"
+    git checkout "$(git branch | cut -c 3- | fzf --preview="git log {} --")"
   fi
 }
 
@@ -34,12 +33,12 @@ function git-add {
 # Create a slug from a string
 # https://gist.github.com/oneohthree/f528c7ae1e701ad990e6
 function slugify {
-    echo "$@" | iconv -t ascii//TRANSLIT | sed -E 's/[^a-zA-Z0-9]+/-/g' | sed -E 's/^-+\|-+$//g' | tr '[:upper:]' '[:lower:]'
+  echo "$@" | iconv -t ascii//TRANSLIT | sed -E 's/[^a-zA-Z0-9]+/-/g' | sed -E 's/^-+\|-+$//g' | tr '[:upper:]' '[:lower:]'
 }
 
 function git-checkout-upsert {
   BRANCH=$(slugify $@)
-	git checkout $BRANCH 2>/dev/null || git checkout -b $BRANCH;
+  git checkout $BRANCH 2>/dev/null || git checkout -b $BRANCH
 }
 
 function gcm {
@@ -50,9 +49,21 @@ function grm {
   git rebase $(git_guess_main_branch_name)
 }
 
+function gprm {
+  CURRENT_BRANCH=$(git branch --show-current)
+  gcm
+  git checkout $CURRENT_BRANCH
+  grm
+}
+
+function git-rebase-continue {
+  git add -A
+  git rebase --continue
+}
+
 function git-delete-squashed {
-  TARGET_BRANCH=`git_guess_main_branch_name`
-  
+  TARGET_BRANCH=$(git_guess_main_branch_name)
+
   git checkout -q $TARGET_BRANCH && git for-each-ref refs/heads/ "--format=%(refname:short)" | while read branch; do mergeBase=$(git merge-base $TARGET_BRANCH $branch) && [[ $(git cherry $TARGET_BRANCH $(git commit-tree $(git rev-parse $branch\^{tree}) -p $mergeBase -m _)) == "-"* ]] && git branch -D $branch; done
 }
 
@@ -74,7 +85,6 @@ alias gs="git status"
 alias gcam="git add -A && _git-commit-and-message"
 
 alias _git-root="git rev-parse --show-toplevel"
-
 
 ###############################################################
 # Intuitive map function
@@ -111,7 +121,7 @@ function homesick_commit {
 ###############################################################
 # find file in current dir
 ###############################################################
-alias spot="mdfind -onlyin `pwd`"
+alias spot="mdfind -onlyin $(pwd)"
 
 ###############################################################
 # command aliases
@@ -121,7 +131,6 @@ alias local-ip="ipconfig getifaddr en1"
 alias chrome='open -a Google\ Chrome'
 alias download="curl -O"
 alias grep='grep --color=auto'
-
 
 ###############################################################
 # DOCKER
