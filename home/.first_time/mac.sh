7119fc7846b1d130082fd65654127f55f79abf73
David Blackman <blackmad@Davids-MacBook-Pro.local>
2023-06-20 20:32:03 -0400

    new mac

--- home/.first_time/mac.sh
+++ home/.first_time/mac.sh
@@ -1,1 +1,12 @@
 defaults write com.google.Chrome AppleEnableSwipeNavigateWithScrolls -bool FALSE
+
+mkdir ~/.tools
+cd ~/.tools
+git clone https://github.com/MartinRamm/fzf-docker
+
+mv ~/.oh-my-zsh/custom /tmp
+rm -rf ~/.oh-my-zsh
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+mv /tmp/custom/* ~/.oh-my-zsh/
+
+brew bundle --file=~/.Brewfile 
